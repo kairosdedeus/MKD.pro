7cfcfd12fa2129f567411691376f923da9eb206b
MichaelFelipeCabrera <73919768+MichaelFelipeCabrera@users.noreply.github.com>
2026-05-14 06:01:06 -0400

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-const pagesBasePath = process.env.VITE_BASE_PATH ?? "./";
+const pagesBasePath = process.env.VITE_BASE_PATH ?? "./MKD.pro";
 
 export default defineConfig({
   plugins: [react()],
